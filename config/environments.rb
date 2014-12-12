b4a2a04d983a4e934d4800c8643e4a44b98bfd1b
pam- <pam.assogba@gmail.com>
2014-12-12 09:31:22 -0500

    added rakefile

--- config/environments.rb
+++ config/environments.rb
@@ -1,4 +1,11 @@
 configure :production, :development do
 	db = URI.parse(ENV['DATABASE_URL'] || 'postgresql://localhost/gifty')
-	
+	ActiveRecord::Base.establish_connection(
+		adapter: db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		database: db.path[1..-1],
+		host: db.host,
+		username: db.user,
+		password: db.password,
+		encoding: 'utf8'
+	)
 end 
